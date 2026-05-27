0256c90fdd3185cfd54074a58271aaf1076fbc5e
Ilia <ilia.m.dev@yandex.com>
2026-05-27 20:31:53 +0300

    init project

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
+      '/socket.io': {
+        target: 'http://localhost:5001',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+});
